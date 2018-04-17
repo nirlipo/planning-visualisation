3695d7146222ec6e613f1b33cf06da173b35e2cb
ColinCheng5887 <yyan7@student.unimelb.edu.au>
2018-04-17 16:47:49 +1000

    Upload the basic architecture code

---
+++ PlanningVisualise/Assets/Entity/ProjectVisualizePack/StepItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlanVisualizerArchitecture.Entity.ProjectVisualizePack {
+   public sealed class StepItem {
+
+
+
+   }
+}
