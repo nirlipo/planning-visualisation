63f69d7d997cf0804aaa5f767e5ecfa97ce07435
Ella <>
2018-04-28 22:27:21 +1000

    Add "back" button on visualisation page& add select file

--- PlanningVisualise/Assets/AnimationProfile/Script/ScreenManager.cs
+++ PlanningVisualise/Assets/AnimationProfile/Script/ScreenManager.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class ScreenManager : MonoBehaviour {
-	// Use this for initialization
-	void Start () {
-		DontDestroyOnLoad (this);
-	}
-	public void loadVisualiseScene(){
-		SceneManager.LoadScene ("Start");
-	}
+public class ScreenManager : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+    public void loadVisualiseScene()
+    {
+        SceneManager.LoadScene("Start");
+    }
+    public void loadMainScene(){
+        SceneManager.LoadScene("PlanningVisualise");
+    }
 
 }
