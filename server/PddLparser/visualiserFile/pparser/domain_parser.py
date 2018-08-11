3c6171d58b24eabee34865b6140c83f5460094f2
Ella <1018400831@qq.com>
2018-08-11 22:52:18 +1000

    test cases update and add comments for domain_parser

--- server/PddLparser/visualiserFile/pparser/domain_parser.py
+++ server/PddLparser/visualiserFile/pparser/domain_parser.py
@@ -1,26 +1,27 @@
 import re
-import string
 
 
 #######################################################
 # Input File: domain file
-# Output : predicates for INIT stage and GOAL stage
+# Output : all predicates in the domain
 #######################################################
 def get_domain_json(file_name):
     patternPare = re.compile(r'\((.*?)\)')
     fileop = open(file_name)
     strfile = fileop.read()
     strPre = strfile[strfile.index("predicates") + len("predicates"):strfile.index("action")]
-
+    # find all the predicates in the domain
     namePare = patternPare.findall(strPre)
     PredicateList = {}
     for name in namePare:
+        # predicates with "?"
         if (name.find("?") != -1):
             indexQue = name.find("?")
             namePre = name[0:indexQue - 1]
             #PredicateList.append(namePre)
             PredicateList[namePre] = name.count("?")
-            #print(name, name.count("?"))
+
+        # predicates without "?"
         else:
             #PredicateList.append(name)
             PredicateList[name] = name.count("?")
