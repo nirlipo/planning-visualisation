83abec459d03fcdbe7749cd3bdec0172ad6dca32
syedm1 <syedm1@student.unimelb.edu.au>
2018-08-16 04:08:25 +1000

    Loading with circular animation done

---
+++ PlanningVisualise/Assets/Visualiser/Scripts/circleRotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class circleRotator : MonoBehaviour {
+
+
+
+    private RectTransform rectComponent;
+    private float rotateSpeed = 400f;
+
+    private void Start()
+    {
+        rectComponent = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        rectComponent.Rotate(0f, 0f, rotateSpeed * Time.deltaTime);
+    }
+}
