d6d52c506ca691c4d37f8c1a1e483b6eee60e644
Hugo Chau <openh79q83@gmail.com>
2018-09-13 23:23:21 +1000

    Editor file

---
+++ PlanningVisualise/Assets/Editor/buildscript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class buildscript{
+
+	static void build(){
+		string[] scene = { 
+			"Assets/Visualiser/Scenes/Landing Page.unity", 
+			"Assets/Visualiser/Scenes/Start.unity",
+			"Assets/Visualiser/Scenes/Visualisation.unity",
+			"Assets/Visualiser/Scenes/NetworkError.unity"
+		};
+		string path = "buildweb/";
+		BuildPipeline.BuildPlayer (scene, path, BuildTarget.WebGL, BuildOptions.None);
+	}
+}
